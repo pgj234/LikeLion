d2fce198bc87f0e0ba4f79ce841182947f38ac4c
pgj234 <pgj94@naver.com>
2025-02-21 16:02:14 +0900

    studyReadLine

---
+++ studyReadLine/studyReadLine/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 사용자 입력을 문자열 받기
+            //Console.Write("이름을 입력하세요 : ");
+            //string username = Console.ReadLine();       // 사용자로부터 입력받기
+
+            //Console.Write($"안녕하세요, {username}님!");
+
+            //// 문자열을 정수로 변환
+            //Console.Write("나이를 입력하세요 : ");
+            //string input = Console.ReadLine();      // 사용자로부터 입력받기
+            //int age = int.Parse(input);             // 문자열을 정수로 변환
+
+            //Console.WriteLine(age + "살 이군요!");                      // 변환된 값 사용
+            //Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");
+            //Console.WriteLine("내년에는 {0}살이 되겠군요!", age+1);
+
+
+            //float ruinSkillDmgNum;              // 루인 스킬 피해
+            //float cardGaugeGetNum;              // 카드 게이지 획득량
+            //float ultimateDmgNum;               // 각성기 피해
+            //int maxMpNum;                       // 최대 마나
+            //int fightingMPRegenerationNum;      // 전투 중 마나 회복량
+            //int notFightingMPRegenerationNum;   // 비전투 중 마나 회복량
+            //float moveSpdNum;                   // 이동 속도
+            //float vehicleMoveSpdNum;            // 탈 것 속도
+            //float transportSpdNum;              // 운반 속도
+            //float cooltimeReduceNum;            // 스킬 재사용 대기시간 감소
+
+            //Console.Write("루인 스킬 피해(float) : ");
+            //string ruinSkillDmg = Console.ReadLine();
+            //ruinSkillDmgNum = float.Parse(ruinSkillDmg);
+            //Console.WriteLine("");
+
+            //Console.Write("카드 게이지 획득량(float) : ");
+            //string cardGaugeGet = Console.ReadLine();
+            //cardGaugeGetNum = float.Parse(cardGaugeGet);
+            //Console.WriteLine("");
+
+            //Console.Write("각성기 피해(float) : ");
+            //string ultimateDmg = Console.ReadLine();
+            //ultimateDmgNum = float.Parse(ultimateDmg);
+            //Console.WriteLine("");
+
+            //Console.Write("최대 마나(int) : ");
+            //string maxMP = Console.ReadLine();
+            //maxMpNum = int.Parse(maxMP);
+            //Console.WriteLine("");
+
+            //Console.Write("전투 중 마나 회복량(int) : ");
+            //string fightingMPRegeneration = Console.ReadLine();
+            //fightingMPRegenerationNum = int.Parse(fightingMPRegeneration);
+            //Console.WriteLine("");
+
+            //Console.Write("비 전투 중 마나 회복량(int) : ");
+            //string notFightingMPRegeneration = Console.ReadLine();
+            //notFightingMPRegenerationNum = int.Parse(notFightingMPRegeneration);
+            //Console.WriteLine("");
+
+            //Console.Write("이동 속도(float) : ");
+            //string moveSpd = Console.ReadLine();
+            //moveSpdNum = float.Parse(moveSpd);
+            //Console.WriteLine("");
+
+            //Console.Write("탈 것 속도(float) : ");
+            //string vehicleMoveSpd = Console.ReadLine();
+            //vehicleMoveSpdNum = float.Parse(vehicleMoveSpd);
+            //Console.WriteLine("");
+
+            //Console.Write("운반 속도(float) : ");
+            //string transportSpd = Console.ReadLine();
+            //transportSpdNum = float.Parse(transportSpd);
+            //Console.WriteLine("");
+
+            //Console.Write("스킬 재사용 대기시간 감소(float) : ");
+            //string cooltimeReduce = Console.ReadLine();
+            //cooltimeReduceNum = float.Parse(cooltimeReduce);
+            //Console.WriteLine("");
+
+            //Console.Clear();
+
+            //Console.WriteLine("-------------------------------------------------------------------------------");
+            //Console.WriteLine($"루인 스킬 피해 : {ruinSkillDmgNum}%");
+            //Console.WriteLine($"카드 게이지 획득량 : {cardGaugeGetNum}%");
+            //Console.WriteLine($"각성기 피해 : {ultimateDmgNum}%");
+            //Console.WriteLine($"최대 마나 : {maxMpNum}");
+            //Console.WriteLine($"전투 중 마나 회복량 : {fightingMPRegenerationNum}");
+            //Console.WriteLine($"비 전투 중 마나 회복량 : {notFightingMPRegenerationNum}");
+            //Console.WriteLine($"이동 속도 : {moveSpdNum}%");
+            //Console.WriteLine($"탈 것 속도 : {vehicleMoveSpdNum}%");
+            //Console.WriteLine($"운반 속도 : {transportSpdNum}%");
+            //Console.WriteLine($"스킬 재사용 대기시간 감소 : {cooltimeReduceNum}%");
+            //Console.WriteLine("-------------------------------------------------------------------------------");
+        }
+    }
+}
