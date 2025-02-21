be019e82ff77a6a53538d74cebc056c36e9ee362
pgj234 <pgj94@naver.com>
2025-02-21 15:04:26 +0900

    Loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000);         // 1000이 1초 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("총 든 링");
+
+            Console.WriteLine("스토리1");
+        }
+    }
+}
